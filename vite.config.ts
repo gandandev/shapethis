d14462c657d5e302d05f3546a014e58d320b33eb
gandan <188059221+gandandev@users.noreply.github.com>
2025-03-30 18:32:58 +0900

    chore(deps): Phosphor 아이콘 사용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { sveltePhosphorOptimize } from 'phosphor-svelte/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()]
+  plugins: [sveltePhosphorOptimize(), tailwindcss(), sveltekit()]
 })
